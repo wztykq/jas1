7d92a40e1fa2fe4810f416d9a7a619daf5350220
wztykq <43814633+wztykq@users.noreply.github.com>
2019-04-16 11:21:07 +0800

    444

---
+++ dom/a.js
@@ -0,0 +1,7 @@
+
+  function init () {
+    var divTag = document.getElementById('fangkuai')
+    divTag.innerHTML = ''
+    divTag.setAttribute('style','height:100px;width:150px;background-color:salmon;margin-left:400px;margin-top:400px;')
+  }
+  window.onload = init
